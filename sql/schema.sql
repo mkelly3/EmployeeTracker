b40975361abb8b52acd1d934702c228f8c2df356
Morgan Kelly <mkelly5380@gmail.com>
2022-10-30 14:10:02 -0500

    created a new employeeTracker_db and added a department table

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,12 +1,20 @@
 --Create a Courese Database--
+DROP DATABASE IF EXISTS employeeTracker_db;
 
+--create an employeeTracker_db and use it--
+CREATE DATABASE employeeTracker_db;
 
---create a department table--
+USE employeeTracker_db;
 
-
---create roles table--
+--create deparment table--
+DROP TABLE IF EXISTS department(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
 
 --In the roles table need an id that relates to the primary key, title, salary, department_id--
+
 
 --create employess table --
 --in the employees table need id, first_name, last_name, role_id, manager_id"
