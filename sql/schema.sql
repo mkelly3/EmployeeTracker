b3f11adba0da05e979ab96d0b667808016ec34c9
Morgan Kelly <mkelly5380@gmail.com>
2022-10-30 14:05:58 -0500

    added console table npm and pseudocode for schema.sql

---
+++ sql/schema.sql
@@ -0,0 +1,12 @@
+--Create a Courese Database--
+
+
+--create a department table--
+
+
+--create roles table--
+
+--In the roles table need an id that relates to the primary key, title, salary, department_id--
+
+--create employess table --
+--in the employees table need id, first_name, last_name, role_id, manager_id"
