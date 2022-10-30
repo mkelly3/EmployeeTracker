9c68de413a6a554751c6221efbb7e5034c9ba9b0
Morgan Kelly <mkelly5380@gmail.com>
2022-10-30 15:08:27 -0500

    added a primary key to the roles and employees table in schema.sql

--- server.js
+++ server.js
@@ -59,7 +59,6 @@ function onFirstQuestion(){
         }
 
     });
-
 }
 
 function viewAllDepartment(){
--- sql/schema.sql
+++ sql/schema.sql
@@ -9,8 +9,8 @@ USE employees_db;
 --create deparment table--
 DROP TABLE IF EXISTS department;
 CREATE TABLE department(
-id INT auto_increment KEY NOT NULL,
-name VARCHAR(30),
+id INT NOT NULL auto_increment,
+name VARCHAR(30) NOT NULL,
 PRIMARY KEY(id)
 );
 
@@ -22,8 +22,7 @@ id INT auto_increment PRIMARY KEY NOT NULL,
 title VARCHAR(30),
 salary DECIMAL(10),
 department_id INT,
-FOREIGN KEY (department_id),
-REFERENCES department(id) ON DELETE CASCADE
+FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 --create employess table --
@@ -33,10 +32,10 @@ DROP TABLE IF EXISTS employee;
 
 CREATE TABLE employee(
 id INT auto_increment PRIMARY KEY NOT NULL,
-first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30) NOT NULL,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
 role_id INT NOT NULL,
 manager_id INT,
-FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
 FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
